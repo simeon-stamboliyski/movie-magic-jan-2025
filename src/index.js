43cfbb55f3262b08dd82bcf2de80fb6f85daf8dc
Simeon Stamboliyski <simeon062007@gmail.com>
2025-01-18 09:45:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
-port = 3000;
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('It works!');
+});
 
 app.listen(port, console.log(`The app is running on http://localhost:3000`));
