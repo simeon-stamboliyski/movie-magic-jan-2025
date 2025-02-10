05d83b77de50ee1740b4a6887d1e6a12baff27b7
Simeon Stamboliyski <simeon062007@gmail.com>
2025-02-11 01:54:06 +0200

    fixed the register system

---
+++ src/controllers/auth-controller.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import authService from "../services/auth-service.js";
+
+const authController = Router();
+
+authController.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+authController.post('/register', async (req, res) => {
+    const userData = req.body;
+
+    await authService.register(userData);
+
+    res.end();
+});
+
+export default authController;
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import homeRouter from './controllers/home-controller.js';
 import movieRouter from './controllers/movie-controller.js';
 import castRouter from './controllers/cast-controller.js';
+import authRouter from './controllers/auth-controller.js';
 
 const router = express.Router();
 
@@ -10,6 +11,8 @@ router.use(homeRouter);
 router.use('/movies', movieRouter);
 
 router.use('/casts', castRouter);
+
+router.use('/auth', authRouter);
 
 router.get('*', (req, res) => {
     res.render('404');
