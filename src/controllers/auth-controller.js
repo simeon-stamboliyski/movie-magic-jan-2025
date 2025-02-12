5cd4b27fa312a850d706e3b5eee8f7cd705ae90b
Simeon Stamboliyski <simeon062007@gmail.com>
2025-02-12 21:38:24 +0200

    idk

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -19,4 +19,17 @@ authController.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
+authController.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        await authService.login(email, password);
+        return res.redirect('/');
+    }
+    catch (err) { 
+        console.log(err.message); 
+        return res.redirect('/404'); 
+    }
+});
+
 export default authController;
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,7 +1,21 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 export default {
     register(userData) {
         return User.create(userData);
+    },
+    async login(email, password) {
+        const user = await User.findOne({ email }); 
+
+        if (!user) {
+            throw new Error('Invalid email');
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+
+        if (!isValid) {
+            throw new Error('Invalid password');
+        }
     }
 }
