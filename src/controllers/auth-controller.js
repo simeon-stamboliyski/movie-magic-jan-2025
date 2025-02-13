7d16e2c2b0bc10c3f0ac6866663d309527c22868
Simeon Stamboliyski <simeon062007@gmail.com>
2025-02-13 02:03:28 +0200

    add logout func

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -34,4 +34,10 @@ authController.post('/login', async (req, res) => {
     }
 });
 
+authController.get('/logout', (req, res) => {
+    res.clearCookie('auth');
+
+    res.redirect('/');
+});
+
 export default authController;
